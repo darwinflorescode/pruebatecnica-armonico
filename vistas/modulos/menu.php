d76c79a6f876a2bee11f7d764e6338d850ba7028
Darwin Flores <alfonso.darwinflores96@gmail.com>
2023-06-28 05:01:16 -0600

    Módulo de Usuario Modificado - 1 hora empleada en configurarlo

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,42 @@
+<aside class="main-sidebar">
+
+	 <section class="sidebar">
+
+		<ul class="sidebar-menu">
+
+		<?php
+
+		if($_SESSION["perfil"] == "Administrador"){
+
+			echo '<li class="active">
+
+				
+
+			</li>
+
+			<li>
+
+				<a href="usuarios">
+
+					<i class="fa fa-user"></i>
+					<span>Usuarios</span>
+
+				</a>
+
+			</li>';
+
+		}
+
+		
+
+		
+
+		
+
+		?>
+
+		</ul>
+
+	 </section>
+
+</aside>
